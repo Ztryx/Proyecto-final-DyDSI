c06a5ae41307ab766d724b24b10e5ec04b86cf10
= <=>
2015-01-19 17:55:11 +0100

    Añadida funcionalidad de comprobar si un voluntario existe en la base de datos

--- test/Persistencia/TestManejaVoluntario.java
+++ test/Persistencia/TestManejaVoluntario.java
@@ -4,10 +4,20 @@
  */
 package Persistencia;
 
+import Aplicacion.ConexionOracle;
+
 /**
  *
  * @author usuario
  */
 public class TestManejaVoluntario {
-    
+    public static void main(String[] args) {
+        ConexionOracle conn = new ConexionOracle();
+        conn.Conexion();
+        
+        ManejaVoluntario mVol = new ManejaVoluntario(conn);
+        mVol.eliminaVoluntario(1);
+        int id = mVol.generarClave();
+        
+    }
 }
