32b1a5716c0fb3f69670910fb55b98f18f5ad8b0
Truji92 <alejandro.trujillo.7@gmail.com>
2015-01-19 16:43:20 +0100

    Añadida funcion getInstituciones

--- src/Persistencia/ManejaInstitucion.java
+++ src/Persistencia/ManejaInstitucion.java
@@ -10,6 +10,8 @@ import Datos.Institucion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -20,11 +22,31 @@ public class ManejaInstitucion extends ManejaTabla {
     public ManejaInstitucion(ConexionOracle conn) {
         super(conn);
     }
-    
+
+    public void mostrarInstituciones() {
+        try (Statement stmt = conn.createStatement()) {
+            String statement = "select * from INSTITUCION";
+            ResultSet rs = stmt.executeQuery(statement);
+            List<Institucion> instituciones = new LinkedList<>();
+            while (rs.next()) {
+                instituciones.add(new Institucion(rs.getString("cif"),
+                        rs.getString("nombre"),
+                        rs.getString("razon_social"),
+                        rs.getString("telefono"),
+                        rs.getInt("idVoluntario")));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al consultar la tabla INSTITUCION");
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+            System.out.println(e.getErrorCode());
+        }
+    }
+
     public void eliminaInstitucion(String cif) {
         try (Statement stmt = conn.createStatement()) {
-            String statement = "delete from INSTITUCION where CIF=" +
-                    "'" + cif+ "'";
+            String statement = "delete from INSTITUCION where CIF="
+                    + "'" + cif + "'";
             stmt.executeUpdate(statement);
             conn.commit();
         } catch (SQLException e) {
@@ -36,7 +58,7 @@ public class ManejaInstitucion extends ManejaTabla {
         }
         int idVoluntario = -1;
         try (Statement stmt = conn.createStatement()) {
-            String statement = "select idVoluntario from INSTITUCION where CIF=" 
+            String statement = "select idVoluntario from INSTITUCION where CIF="
                     + "'" + cif + "'";
             ResultSet rs = stmt.executeQuery(statement);
             rs.next();
@@ -47,20 +69,20 @@ public class ManejaInstitucion extends ManejaTabla {
             System.out.println(e.getSQLState());
             System.out.println(e.getErrorCode());
         }
-        
+
         ManejaVoluntario mVol = new ManejaVoluntario(conn);
         mVol.eliminaVoluntario(idVoluntario);
     }
-    
+
     public Institucion getVoluntario(int idVoluntario) {
         Institucion instit = null;
-        try(Statement stmt = conn.createStatement()) {
+        try (Statement stmt = conn.createStatement()) {
             String statement = "select * from INSTITUCION "
                     + "where idVoluntario='" + idVoluntario + "'";
             ResultSet rs = stmt.executeQuery(statement);
             rs.next();
             instit = new Institucion(rs.getString("cif"),
-                    rs.getString("nombre"), 
+                    rs.getString("nombre"),
                     rs.getString("razon_social"),
                     rs.getString("telefono"),
                     idVoluntario);
@@ -73,11 +95,11 @@ public class ManejaInstitucion extends ManejaTabla {
         }
         return instit;
     }
-    
+
     public void cambiaCIF(String cif_actual, String cif_nuevo) {
         try (Statement stmt = conn.createStatement()) {
-            String statement = "update INSTITUCION set CIF='" +
-                    cif_nuevo + "' where CIF='" + cif_actual + "'";
+            String statement = "update INSTITUCION set CIF='"
+                    + cif_nuevo + "' where CIF='" + cif_actual + "'";
             stmt.executeUpdate(statement);
             conn.commit();
         } catch (SQLException e) {
@@ -88,7 +110,7 @@ public class ManejaInstitucion extends ManejaTabla {
             System.out.println(e.getErrorCode());
         }
     }
-    
+
     public int generarClave() {
         String statement = "SELECT MAX(id) FROM INSTITUCION";
         int maximaClave = -1;
@@ -97,7 +119,7 @@ public class ManejaInstitucion extends ManejaTabla {
             ResultSet rs = stmt.executeQuery(statement);
             maximaClave = rs.getInt(1);
             maximaClave++;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error al consultar clave de institucion");
             System.out.println(ex.getMessage());
             System.out.println(ex.getSQLState());
