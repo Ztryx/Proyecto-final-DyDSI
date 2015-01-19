3d4facab29f614954425a979ae8d923805d27916
Truji92 <alejandro.trujillo.7@gmail.com>
2015-01-19 12:39:26 +0100

    Añadidos generadores de claves y clase menu

--- src/Persistencia/ManejaEstablecimiento.java
+++ src/Persistencia/ManejaEstablecimiento.java
@@ -6,6 +6,9 @@
 package Persistencia;
 
 import Aplicacion.ConexionOracle;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -17,4 +20,20 @@ public class ManejaEstablecimiento extends ManejaTabla {
         super(conn);
     }
     
+    public int generarClave() {
+        String statement = "SELECT MAX(id) FROM ESTABLECIMIENTO";
+        int maximaClave = -1;
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(statement);
+            maximaClave = rs.getInt(1);
+            maximaClave++;
+        } catch (SQLException ex){
+            System.out.println("Error al consultar clave de establecimiento");
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getSQLState());
+            System.out.println(ex.getErrorCode());
+        }
+        return maximaClave;
+    }
 }
